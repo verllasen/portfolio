cf967d510f88677fa84c72ab6b83c8cddec4b6fe
verllasen <kaunkirill187@gmail.com>
2026-03-27 01:55:34 +0300

    Add files via upload

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -23,7 +23,7 @@ function createWindow() {
     frame: false,
     backgroundColor: '#000000', // Pure black
     webPreferences: {
-      preload: path.join(__dirname, 'preload.mjs'),
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: false,
       contextIsolation: true,
     },
