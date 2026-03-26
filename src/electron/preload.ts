4301a1b46d72f3ab7590fb308efec2beceb4836f
verllasen <kaunkirill187@gmail.com>
2026-03-27 02:16:07 +0300

    Add files via upload

--- src/electron/preload.ts
+++ src/electron/preload.ts
@@ -1,7 +1,13 @@
-import { contextBridge, ipcRenderer } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('electron', {
+  minimize: () => ipcRenderer.send('window-minimize'),
+  maximize: () => ipcRenderer.send('window-maximize'),
+  close: () => ipcRenderer.send('window-close'),
+});
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  windowMinimize: () => ipcRenderer.send('window-min'),
-  windowMaximize: () => ipcRenderer.send('window-max'),
+  windowMinimize: () => ipcRenderer.send('window-minimize'),
+  windowMaximize: () => ipcRenderer.send('window-maximize'),
   windowClose: () => ipcRenderer.send('window-close'),
-})
+});
