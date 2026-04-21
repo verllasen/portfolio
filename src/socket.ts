ed1db4bd130f27dbd47877ef7e78bd6ece6e6d9d
verllasen <kaunkirill187@gmail.com>
2026-04-21 14:36:45 +0300

    Delete src directory

--- src/socket.ts
+++
@@ -1,7 +0,0 @@
-import { io } from 'socket.io-client';
-
-const SOCKET_URL = import.meta.env.PROD ? '/' : 'http://localhost:3001';
-
-export const socket = io(SOCKET_URL, {
-  autoConnect: true,
-});
